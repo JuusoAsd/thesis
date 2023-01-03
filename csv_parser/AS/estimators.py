82be32e5abba228ce5ea34607627060d0a5e1474
JuusoAsd <jahlroos96@gmail.com>
2023-01-03 10:35:51 +0200

    updated vol and intensity estimating

--- csv_parser/AS/estimators.py
+++ csv_parser/AS/estimators.py
@@ -9,13 +9,23 @@ def curve_func(t, a, b):
 
 # you can run this by running python -m pytest tests/test_as.py -v -s
 class IntensityEstimator:
-    def __init__(self, lookback):
+    """
+    lookback: how far back (in milliseconds) to look at trades, default: 1 day
+    update_interval: how often to update the estimator (in milliseconds), default: 1 minute
+
+    lookback is a rough estimate that acts as upper bound on how far back we look at trades,
+    when we are past the lookback period, we switch to shorter set of trades and start estimating intenstity from that
+    """
+
+    def __init__(self, lookback=(1000 * 60 * 60 * 24), update_interval=(1000 * 60)):
         self.lookback = lookback
+        self.update_interval = update_interval
+        self.previous_update = 0
         self.lookback_start_time = 0
         self.trades = {}
         self.trades_short = {}
-        self.kappa = 2
-        self.alpha = 2
+        self.kappa = 1
+        self.alpha = 1
         self.trade_count = 0
 
     # we start by updating main estimator with trades and recording the value every record_interval
@@ -61,88 +71,135 @@ class IntensityEstimator:
         for i in price_levels:
             lambdas.append(self.trades[i])
         lambdas = np.array(lambdas)
-        param, _ = curve_fit(
-            f=curve_func,
-            xdata=price_levels,
-            ydata=lambdas,
-            p0=(self.alpha, self.kappa),
-            method="dogbox",
-            bounds=([0, 0], [np.inf, np.inf]),
-        )
-        self.alpha = param[0]
-        self.kappa = param[1]
-
+        try:
+            param, _ = curve_fit(
+                f=curve_func,
+                xdata=price_levels,
+                ydata=lambdas,
+                p0=(self.alpha, self.kappa),
+                method="dogbox",
+                bounds=([0, 0], [np.inf, np.inf]),
+            )
+            self.alpha = param[0]
+            self.kappa = param[1]
+        except RuntimeError as e:
+            logging.error(f"Failed estimating parameters for intensity")
+        except Exception as e:
+            logging.error(f"Error fitting curve for intensity")
+            logging.error(f"price_levels: {price_levels}")
+            logging.error(f"lambdas: {lambdas}")
+            raise e
         if self.alpha <= 0:
             self.alpha = 1
         if self.kappa <= 0:
             self.kappa = 1
 
-    # OLD: comes from hummingbot
-    def calculate(self, ts, midprice):
-
-        price = price
-        # Descending order of price-timestamp quotes
-        self._last_quotes = [
-            {"timestamp": timestamp, "price": price}
-        ] + self._last_quotes
-
-        latest_processed_quote_idx = None
-        # iterate over trades
-        for trade in self._current_trade_sample:
-            # iterate over quotes
-            for i, quote in enumerate(self._last_quotes):
-                # if quote happened before trade
-                if quote["timestamp"] < trade.timestamp:
-                    # if quote happened before latest processed quote
-                    if (
-                        latest_processed_quote_idx is None
-                        or i < latest_processed_quote_idx
-                    ):
-                        latest_processed_quote_idx = i
-                    trade = {
-                        "price_level": abs(trade.price - float(quote["price"])),
-                        "amount": trade.amount,
-                    }
-
-                    if quote["timestamp"] + 1 not in self._trade_samples.keys():
-                        self._trade_samples[quote["timestamp"] + 1] = []
-
-                    self._trade_samples[quote["timestamp"] + 1] += [trade]
-                    break
-
-        # THere are no trades left to process
-        self._current_trade_sample = []
-        # Store quotes that happened after the latest trade + one before
-        if latest_processed_quote_idx is not None:
-            self._last_quotes = self._last_quotes[0 : latest_processed_quote_idx + 1]
-
-        if len(self._trade_samples.keys()) > self._sampling_length:
-            timestamps = list(self._trade_samples.keys())
-            timestamps.sort()
-            timestamps = timestamps[-self._sampling_length :]
-
-            trade_samples = {}
-            for timestamp in timestamps:
-                trade_samples[timestamp] = self._trade_samples[timestamp]
-            self._trade_samples = trade_samples
-
-        if self.is_sampling_buffer_full:
-            self.c_estimate_intensity()
+    def get_current_estimate(self, ts):
+        if ts >= self.previous_update + self.update_interval:
+            self.calculate_current_values()
+            self.previous_update = ts
+        return self.alpha, self.kappa
 
 
 class VolatilityEstimator:
-    def __init__(self):
-        self.prices = []
-        self.volatility = 0
-        self.prices_count = 0
+    """
+    lookback: how far back (in milliseconds) to look at trades, default: 1 day
+    return_aggregation: aggregation period for price returns (what is the time period between two price observations), default: 10 minutes
+    update_interval: how often to update the estimator (in milliseconds), default: 1 minute
+    """
 
-    def update_prices(self, new_price):
-        # new_price is a variable containing one new price
-        self.prices.append(new_price)
-        self.prices_count += 1
+    def __init__(
+        self,
+        lookback=(1000 * 60 * 60 * 24),
+        return_aggregation=(1000 * 60 * 10),
+        update_interval=(1000 * 60),
+    ):
+        self.lookback = lookback
+        self.return_aggregation = return_aggregation
+        self.update_interval = update_interval
+
+        self.previous_price = 0
+        self.previous_return_ts = 0
+
+        self.returns = []
+        self.return_count = 0
+
+        self.volatility = 0
+        self.previous_update = 0
+
+    def update_prices(self, new_price, ts):
+        if self.previous_price == 0:
+            self.previous_price = new_price
+            self.previous_return_ts = ts
+        elif ts >= self.previous_return_ts + self.return_aggregation:
+            ret = new_price / self.previous_price - 1
+            self.returns.append(ret)
+            self.return_count += 1
+            self.previous_price = new_price
+            self.previous_return_ts = ts
 
     def calculate_volatility(self):
-        # TO DO: Deciding sample size.. Similar to intensity calculations probably?
-        price_sample = np.array(self.prices)
-        vol = np.sqrt(np.sum(np.square(np.diff(price_sample)))) / self.prices_count
-        self.volatility = vol
+        """
+        Vol = std(returns)
+        Annualized vol = vol * sqrt(aggregation periods per year)
+            - aggregation period is in milliseconds
+        """
+        return_arr = np.array(self.returns)
+        vol = np.std(return_arr)
+        self.volatility = vol * np.sqrt(
+            (1000 * 60 * 60 * 24 * 365) / self.return_aggregation
+        )
+
+    def get_current_estimate(self, ts):
+        if ts >= self.previous_update + self.update_interval:
+            self.calculate_volatility()
+            self.previous_update = ts
+        return self.volatility
+
+
+# OLD: comes from hummingbot
+def calculate(self, ts, midprice):
+
+    price = price
+    # Descending order of price-timestamp quotes
+    self._last_quotes = [{"timestamp": timestamp, "price": price}] + self._last_quotes
+
+    latest_processed_quote_idx = None
+    # iterate over trades
+    for trade in self._current_trade_sample:
+        # iterate over quotes
+        for i, quote in enumerate(self._last_quotes):
+            # if quote happened before trade
+            if quote["timestamp"] < trade.timestamp:
+                # if quote happened before latest processed quote
+                if latest_processed_quote_idx is None or i < latest_processed_quote_idx:
+                    latest_processed_quote_idx = i
+                trade = {
+                    "price_level": abs(trade.price - float(quote["price"])),
+                    "amount": trade.amount,
+                }
+
+                if quote["timestamp"] + 1 not in self._trade_samples.keys():
+                    self._trade_samples[quote["timestamp"] + 1] = []
+
+                self._trade_samples[quote["timestamp"] + 1] += [trade]
+                break
+
+    # THere are no trades left to process
+    self._current_trade_sample = []
+    # Store quotes that happened after the latest trade + one before
+    if latest_processed_quote_idx is not None:
+        self._last_quotes = self._last_quotes[0 : latest_processed_quote_idx + 1]
+
+    if len(self._trade_samples.keys()) > self._sampling_length:
+        timestamps = list(self._trade_samples.keys())
+        timestamps.sort()
+        timestamps = timestamps[-self._sampling_length :]
+
+        trade_samples = {}
+        for timestamp in timestamps:
+            trade_samples[timestamp] = self._trade_samples[timestamp]
+        self._trade_samples = trade_samples
+
+    if self.is_sampling_buffer_full:
+        self.c_estimate_intensity()
