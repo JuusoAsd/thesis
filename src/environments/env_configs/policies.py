9c2b84dca73442a0b52f246f4b8c25dba07a099a
JuusoAsd <jahlroos96@gmail.com>
2023-04-10 17:56:20 +0300

    refactor policies

--- src/environments/env_configs/policies.py
+++ src/environments/env_configs/policies.py
@@ -1,7 +1,11 @@
 from abc import ABC
 import numpy as np
 
-from environments.env_configs.spaces import ActionSpace, ObservationSpace
+from environments.env_configs.spaces import (
+    ActionSpace,
+    ObservationSpace,
+    LinearObservation,
+)
 
 
 class PolicyBase(ABC):
@@ -21,21 +25,6 @@ class PolicyBase(ABC):
         self.observation_space = observation_space
         self.action_space = action_space
 
-    def normalize(self, action, mid_price):
-        bid_size = action[0]
-        ask_size = action[1]
-        bid = action[2]
-        ask = action[3]
-
-        bid_size_normalize = bid_size / self.max_order_size
-        ask_size_normalize = ask_size / self.max_order_size
-        bid_normalize = ((bid - mid_price) / self.tick_size) / self.max_ticks
-        ask_normalize = ((ask - mid_price) / self.tick_size) / self.max_ticks
-
-        return np.array(
-            [[bid_size_normalize, ask_size_normalize, bid_normalize, ask_normalize]]
-        )
-
     def get_action(self, state):
         raise NotImplementedError
 
@@ -44,64 +33,6 @@ class PolicyBase(ABC):
 
     def get_observation_space(self):
         return self.observation_space.value
-
-
-class AvellanedaStoikovPolicy(PolicyBase):
-    """
-    Policy that determines bid and ask based on the Avellaneda-Stoikov model
-    params:
-        order_size: size of orders to place
-        inventory_target: inventory target parameter
-        risk_aversion: risk aversion parameter
-    returns:
-        array of bid size, ask size, bid price, ask price
-    """
-
-    def __init__(
-        self,
-        max_order_size,
-        tick_size,
-        max_ticks,
-        price_decimals,
-        observation_space,
-        action_space,
-        order_size,
-        # inventory_target,
-        risk_aversion,
-    ):
-        super().__init__(
-            max_order_size,
-            tick_size,
-            max_ticks,
-            price_decimals,
-            observation_space,
-            action_space,
-        )
-        self.order_size = order_size
-        # self.inventory_target = inventory_target
-        self.risk_aversion = risk_aversion
-        self.observation_space = ObservationSpace.ASObservation.value
-        self.action_space = ActionSpace.NormalizedAction.value
-
-    def get_action(self, obs):
-        obs = obs[0]
-        best_bid = obs[0]
-        best_ask = obs[1]
-        inventory = obs[2]
-        vol = obs[3]
-        intensity = obs[4]
-        mid_price = (best_bid + best_ask) / 2
-        reservation_price = mid_price - inventory * vol * self.risk_aversion
-        spread = vol * self.risk_aversion + 2 / self.risk_aversion * np.log(
-            1 + self.risk_aversion / intensity
-        )
-
-        bid_size = self.order_size
-        ask_size = self.order_size
-        bid = round(reservation_price - spread / 2, self.price_decimals)
-        ask = round(reservation_price + spread / 2, self.price_decimals)
-
-        return self.normalize(np.array([bid_size, ask_size, bid, ask]), mid_price)
 
 
 class ASPolicyVec:
@@ -115,12 +46,14 @@ class ASPolicyVec:
         inventory_target,
         risk_aversion,
         order_size,
+        max_diff=0.001,
         obs_type=ObservationSpace.OSIObservation,
         act_type=ActionSpace.NormalizedAction,
     ):
         self.env = env
         self.max_order_size = max_order_size
         self.tick_size = tick_size
+        self.max_diff = max_diff
         self.max_ticks = max_ticks
         self.price_decimals = price_decimals
         self.inventory_target = inventory_target
@@ -130,6 +63,14 @@ class ASPolicyVec:
         self.act_type = act_type
 
     def get_continuous_action(self, observation):
+        """
+        Continuous action returning:
+        - bid size as a percentage of max order size
+        - ask size as a percentage of max order size
+        - bid price as HALF TICKS from mid price / max half ticks
+        - ask price as HALF TICKS from mid price / max half ticks
+        """
+
         if observation.ndim == 1:
             observation = observation.reshape(1, -1)
         mid_price = observation[:, 0]
@@ -144,49 +85,36 @@ class ASPolicyVec:
 
         bid_size = self.order_size
         ask_size = self.order_size
-        bid = np.round(reservation_price - spread / 2, self.price_decimals)
-        ask = np.round(reservation_price + spread / 2, self.price_decimals)
         bid_size_normalize = bid_size / self.max_order_size
         ask_size_normalize = ask_size / self.max_order_size
-        bid_normalize = ((bid - mid_price) / self.tick_size) / self.max_ticks
-        ask_normalize = ((ask - mid_price) / self.tick_size) / self.max_ticks
+
+        # bid and ask prices (actual)
+        bid = np.round(reservation_price - spread / 2, self.price_decimals)
+        bid_half_ticks = ((bid - mid_price) / self.tick_size) / 2
+        bid_tick_ratio = bid_half_ticks / self.max_ticks
+
+        ask = np.round(reservation_price + spread / 2, self.price_decimals)
+        ask_half_ticks = ((ask - mid_price) / self.tick_size) / 2
+        ask_tick_ratio = ask_half_ticks / self.max_ticks
 
         return np.array(
-            [bid_size_normalize, ask_size_normalize, bid_normalize, ask_normalize]
+            [bid_size_normalize, ask_size_normalize, bid_tick_ratio, ask_tick_ratio]
         ).T
-
-    def get_integer_action(self, observation):
-        # returns same as action float but using integers
-        if observation.ndim == 1:
-            observation = observation.reshape(1, -1)
-        mid_price = observation[:, 0]
-        inventory = observation[:, 1]
-        intensity = observation[:, 2]
-        vol = observation[:, 3]
-
-        reservation_price = mid_price - inventory * vol * self.risk_aversion
-        spread = vol * self.risk_aversion + 2 / self.risk_aversion * np.log(
-            1 + self.risk_aversion / intensity
-        )
-
-        bid_size = self.order_size
-        ask_size = self.order_size
-
-        bid = np.round(reservation_price - spread / 2, self.price_decimals)
-        ask = np.round(reservation_price + spread / 2, self.price_decimals)
-
-        # use half ticks to normalize the size, mid price usually in middle of tick
-        bid_normalize = np.round(((bid - mid_price) / (self.tick_size / 2))).astype(int)
-        ask_normalize = np.round(((ask - mid_price) / (self.tick_size / 2))).astype(int)
-        return np.array([bid_size, ask_size, bid_normalize, ask_normalize]).T
 
     def get_action_no_mid(self, observation):
         """
-        Alternative action space that does not use mid price
-        mid price fluctuates and might be difficult to detect that it is actually sort of irrelevant
+        Alternative action space that does not use mid price. Otherwise same as continuous action
+
         """
         if observation.ndim == 1:
             observation = observation.reshape(1, -1)
+
+        bid_size = self.order_size
+        ask_size = self.order_size
+
+        bid_size_normalize = np.full(self.env.n_envs, bid_size / self.max_order_size)
+        ask_size_normalize = np.full(self.env.n_envs, ask_size / self.max_order_size)
+
         inventory = observation[:, 0]
         vol = observation[:, 1]
         intensity = observation[:, 2]
@@ -195,119 +123,99 @@ class ASPolicyVec:
             1 + self.risk_aversion / intensity
         )
         inventory_adjustment = -inventory * vol * self.risk_aversion
-        bid_size = self.order_size
-        ask_size = self.order_size
-        bid_size_normalize = np.full(self.env.n_envs, bid_size / self.max_order_size)
-        ask_size_normalize = np.full(self.env.n_envs, ask_size / self.max_order_size)
 
-        bid = np.round(inventory_adjustment - spread / 2, self.price_decimals)
-        ask = np.round(inventory_adjustment + spread / 2, self.price_decimals)
+        # relative USD amount from mid price
+        bid = inventory_adjustment - spread / 2
+        # half ticks from mid price
+        # bid_ticks = np.round((bid / self.tick_size) * 2, 0)
+        # normalized w.r.t. max ticks
+        bid_normalize = bid / self.max_diff
 
-        bid_normalize = (bid / self.tick_size) / self.max_ticks
-        ask_normalize = (ask / self.tick_size) / self.max_ticks
-        # print(
-        #     np.array(
-        #         [bid_size_normalize, ask_size_normalize, bid_normalize, ask_normalize]
-        #     ).T
-        # )
-        # exit()
+        ask = inventory_adjustment + spread / 2
+        # ask_ticks = np.round((ask / self.tick_size) * 2, 0)
+        ask_normalize = ask / self.max_diff
+
         return np.array(
             [bid_size_normalize, ask_size_normalize, bid_normalize, ask_normalize]
         ).T
 
-    def get_action_func(self):
-        if self.obs_type == ObservationSpace.OSIObservation:
-            if self.act_type == ActionSpace.NormalizedAction:
-                return self.get_continuous_action
-            elif self.act_type == ActionSpace.IntegerAction:
-                return self.get_integer_action
-        elif self.obs_type == ObservationSpace.SimpleObservation:
-            if self.act_type == ActionSpace.NormalizedAction:
-                return self.get_action_no_mid
-        raise NotImplementedError(
-            "action func not implemented for this obs/act combination"
-        )
-
-
-class AlwaysSamePolicyVec:
-    def __init__(self, env) -> None:
-        self.env = env
-
-    def get_action(self, observation):
+    def get_action_no_mid_no_size(self, observation):
         if observation.ndim == 1:
             observation = observation.reshape(1, -1)
-        # returns same shape action as before to make things easier
-        constant_value = 0.5
-        a = np.full(self.env.n_envs, constant_value)
-        b = np.full(self.env.n_envs, constant_value)
-        c = np.full(self.env.n_envs, constant_value)
-        d = np.full(self.env.n_envs, constant_value)
-
-        return np.array([a, b, c, d]).T
-
-    def get_action_func(self):
-        return self.get_action
-
-
-class SimplePolicyVec:
-    """
-    returns:
-        - absolute value of inventory
-        - inventory / 2
-        - volatility / 2
-        - volatility ** 2
-    """
-
-    def __init__(self, env, **kwargs) -> None:
-        self.env = env
-
-    def get_action(self, observation):
-        try:
-            observation = self.env.unnormalize_obs(observation)
-
-        except AttributeError:
-            pass
-        if observation.ndim == 1:
-            observation = observation.reshape(1, -1)
-        # numbers can be very large, need to normalize to match action space
-        # Can't be a constant function tho
-        # inventory: [-1, 1]
-        # volatility: [0, 1]
-        # intensity: [0, 100_000]
-        # want to fit actions on [0, 1]
-
-        # do not use inventory, might be that BC won't see different inventory states?
-        # inventory = observation[:, 0]
+        inventory = observation[:, 0]
         vol = observation[:, 1]
+        intensity = observation[:, 2]
+        spread = vol * self.risk_aversion + 2 / self.risk_aversion * np.log(
+            1 + self.risk_aversion / intensity
+        )
+        inventory_adjustment = -inventory * vol * self.risk_aversion
 
-        # normalize < 1
-        intensity = observation[:, 2] / 100_000
-        a = vol / 2
-        b = vol**2
-        c = np.abs(intensity)
-        d = np.abs(intensity) / 2
+        bid = inventory_adjustment - spread / 2
+        bid_normalize = bid / self.max_diff
 
-        return np.array([a, b, c, d]).T
+        ask = inventory_adjustment + spread / 2
+        ask_normalize = ask / self.max_diff
+
+        return np.array([bid_normalize, ask_normalize]).T
+
+    def get_no_size_action_linear(self, observation):
+        # the input is normalized
+        obs_dict = {
+            "inventory": observation[:, 0],
+            "volatility": observation[:, 1],
+            "intensity": observation[:, 2],
+        }
+        obs_dict_converted = self.obs_type.convert_to_readable(obs_dict)
+        obs = []
+        for i in ["inventory", "volatility", "intensity"]:
+            obs.append(obs_dict_converted[i])
+        return self.get_action_no_mid_no_size(np.array(obs).T)
 
     def get_action_func(self):
-        return self.get_action
+        action_func_dict = {
+            (
+                ObservationSpace.OSIObservation,
+                ActionSpace.NormalizedAction,
+            ): self.get_continuous_action,
+            (
+                ObservationSpace.SimpleObservation,
+                ActionSpace.NormalizedAction,
+            ): self.get_action_no_mid,
+            (
+                ObservationSpace.SimpleObservation,
+                ActionSpace.NoSizeAction,
+            ): self.get_action_no_mid_no_size,
+            (
+                LinearObservation,
+                ActionSpace.NoSizeAction,
+            ): self.get_no_size_action_linear,
+        }
+        input = (self.obs_type, self.act_type)
+        try:
+            return action_func_dict[input]
+        except KeyError:
+            raise NotImplementedError(
+                "action func not implemented for this obs/act combination"
+            )
 
 
 def convert_continuous_action(env, action):
     # this is not fully continuous but uses float and looks like one
-    # print(action)
     bid_sizes = np.full(env.n_envs, 1)
     ask_sizes = np.full(env.n_envs, 1)
-    bids = action[:, 2] * env.max_ticks * env.tick_size
-    asks = action[:, 3] * env.max_ticks * env.tick_size
-    # print(f"action: {action[:,2]}")
-    # print(f"mid price: {env.mid_price[env.current_step]}")
-    # print(f"bids: {bids}")
-    # print(f"sum bids: {env.mid_price[env.current_step] + bids}")
-    # print(f"bids type: {type(bids)}, {type(bids[0])}")
-    bids_round = np.round(env.mid_price[env.current_step] + bids, env.price_decimals)
-    asks_round = np.round(env.mid_price[env.current_step] + asks, env.price_decimals)
-    return bid_sizes, ask_sizes, bids_round, asks_round
+
+    bid_diff = action[:, 2] * env.max_diff
+    bid_price = env.mid_price[env.current_step] + bid_diff
+    bid_round = np.round(bid_price, env.price_decimals)
+
+    ask_diff = action[:, 3] * env.max_diff
+    ask_price = env.mid_price[env.current_step] + ask_diff
+    ask_round = np.round(ask_price, env.price_decimals)
+
+    # asks = action[:, 3] * env.max_ticks * env.tick_size
+    # bids_round = np.round(env.mid_price[env.current_step] + bids, env.price_decimals)
+    # asks_round = np.round(env.mid_price[env.current_step] + asks, env.price_decimals)
+    return bid_sizes, ask_sizes, bid_round, ask_round
 
 
 def convert_integer_action(env, action):
@@ -328,74 +236,13 @@ def convert_integer_action(env, action):
     return bid_sizes, ask_sizes, bids, asks
 
 
-class RoundingPolicyVec:
-    """
-    returns:
-        vol rounded on 3 decimals * 100
-        vol rounded on 3 decimals * 200
-        vol rounded on 3 decimals * 300
-        vol rounded on 3 decimals * 400
-    """
-
-    def __init__(self, env, **kwargs) -> None:
-        self.env = env
-
-    def get_action(self, observation):
-        try:
-            observation = self.env.unnormalize_obs(observation)
-
-        except AttributeError:
-            pass
-        if observation.ndim == 1:
-            observation = observation.reshape(1, -1)
-        # numbers can be very large, need to normalize to match action space
-        # Can't be a constant function tho
-        # inventory: [-1, 1]
-        # volatility: [0, 1]
-        # intensity: [0, 100_000]
-        # want to fit actions on [0, 1]
-
-        # do not use inventory, might be that BC won't see different inventory states?
-        # inventory = observation[:, 0]
-        vol = np.round(observation[:, 1], 3)
-
-        a = vol * 100
-        b = vol * 200
-        c = vol * 300
-        d = vol * 400
-
-        return np.array([a, b, c, d]).T
-
-    def get_action_func(self):
-        return self.get_action
-
-
-class NThDigitPolicyVec:
-    """
-    returns:
-        sevent digit of vol / 10
-    """
-
-    def __init__(self, env, **kwargs) -> None:
-        self.env = env
-
-    def get_action(self, observation):
-        try:
-            observation = self.env.unnormalize_obs(observation)
-
-        except AttributeError:
-            pass
-        if observation.ndim == 1:
-            observation = observation.reshape(1, -1)
-
-        vol = observation[:, 1] * 1e12
-        vol = np.array([int(x[6]) for x in vol.astype(str)]) / 10
-
-        a = vol
-        b = vol
-        c = vol
-        d = vol
-        return np.array([a, b, c, d]).T
-
-    def get_action_func(self):
-        return self.get_action
+def convert_no_mid_no_size_action(env, action):
+    bids = np.round(
+        (action[:, 0] * env.max_diff) + env.mid_price[env.current_step],
+        env.price_decimals,
+    )
+    asks = np.round(
+        (action[:, 1] * env.max_diff) + env.mid_price[env.current_step],
+        env.price_decimals,
+    )
+    return np.full(env.n_envs, 1), np.full(env.n_envs, 1), bids, asks
