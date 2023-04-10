5afcc1eef32e82e31bcdd85c99ae09986fd04f34
JuusoAsd <jahlroos96@gmail.com>
2023-04-10 17:59:33 +0300

    more

--- src/environments/env_configs/util.py
+++ src/environments/env_configs/util.py
@@ -1,17 +1,6 @@
 from imitation.algorithms.dagger import BetaSchedule
 import numpy as np
 from imitation.algorithms import bc
-from stable_baselines3.common.vec_env import sync_envs_normalization
-from stable_baselines3.common.vec_env import (
-    DummyVecEnv,
-    SubprocVecEnv,
-    VecEnv,
-    VecFrameStack,
-    VecNormalize,
-    VecTransposeImage,
-    is_vecenv_wrapped,
-    VecMonitor,
-)
 from stable_baselines3.common.evaluation import evaluate_policy
 
 
@@ -71,22 +60,12 @@ class BCEvalCallback:
         # don't start evals until "wait" freq periods have passed
         if self.count < self.wait * self.freq:
             return False
-        # if isinstance(self.trainer.venv, VecNormalize):
-        #     try:
-        #         sync_envs_normalization(self.trainer.venv, self.env)
-        #     except AttributeError as e:
-        #         raise AssertionError(
-        #             "Training and eval env are not wrapped the same way, "
-        #             "see https://stable-baselines3.readthedocs.io/en/master/guide/callbacks.html#evalcallback "
-        #             "and warning above."
-        #         ) from e
         reward, _ = evaluate_policy(
             self.trainer.policy,  # type: ignore[arg-type]
             self.env,
             n_eval_episodes=self.n_episodes,
             render=False,
         )
-        # print(reward)
         if self.best_reward < reward:  # improved
             self.best_reward = reward
             self.wait = self.patience
@@ -96,29 +75,6 @@ class BCEvalCallback:
         else:  # no improvement
             self.wait -= 1
             if self.wait <= 0:
-                # if self.verbose:
-                # print(
-                #     f"Stopping training because reward did not improve for {self.patience} consecutive checks"
-                # )
                 return True
             else:
                 return False
-
-
-# import json
-
-
-# def check_config(config):
-#     path = "/home/juuso/Documents/gradu/params/test.json"
-
-#     # read list of dicts from json file, if config exists raise an error, else add config to json file
-#     with open(path, "r") as f:
-#         configs = json.load(f)
-#         if config in configs:
-#             raise ValueError(f"Config {config} already exists")
-#         else:
-#             configs.append(config)
-#             configs = json.dumps(configs)
-
-
-# check_config({"mode": "full", "isActive": False})
